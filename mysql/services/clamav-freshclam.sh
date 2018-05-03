3275477b688c30bd1787adddc313a84fd7762292
Trevor Welsby <trevor@welsby.de>
2018-05-03 14:28:25 +1000

    Send clamav, nginx, syslog, php logs to syslog

--- mysql/services/clamav-freshclam.sh
+++ mysql/services/clamav-freshclam.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 logger -p local3.info -t freshclam "Preparing to start freshclam"
 
+# Send logs to syslog
+sed -i "s/^LogSyslog.*/LogSyslog true/" /etc/clamav/freshclam.conf
+sed "s/^UpdateLogFile/#UpdateLogFile/" /etc/clamav/freshclam.conf
+
 # Wait until database files created
 logger -p local3.info -t freshclam "Waiting for database files to download"
 while [ ! -e /var/lib/clamav/main.cvd ] && [ ! -e /var/lib/clamav/bytecode.cvd ] && [ ! -e /var/lib/clamav/daily.cvd ]; do
